41ece0948ccf09e029221954e20b566cc2317074
AhmadGibran125 <ag3116213@gmail.com>
2024-09-02 17:41:39 +0800

    Membuat Project dan Class

---
+++ Day01.java
@@ -0,0 +1,9 @@
+package Coding100Days;
+
+public class Day01 {
+    public static void main(String[] args) {
+        System.out.println("Ini adalah Hari Pertama");
+        System.out.println("Go 100 Days Coding");
+    }
+    
+}
