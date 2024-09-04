91a6e5979ecdf29f19a9f88f360dd0fa4e250aaf
AhmadGibran125 <ag3116213@gmail.com>
2024-09-04 10:08:54 +0800

    Membuat Ststem.out.println

---
+++ Day03.java
@@ -0,0 +1,11 @@
+package Coding100Days;
+
+public class Day03 {
+     public static void main(String[] args) {
+         System.out.println("Day03 Coding") ;
+         System.out.println("Nama : Ahmad Gibran") ;
+         System.out.println("Nim : D0224330") ;
+         System.out.println("Kelas : D") ;
+         System.out.println("Alamat : Palece") ;
+    }
+}
