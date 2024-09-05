b5f26d0df2227cf405d21cfaf2c7dbea9222399c
AhmadGibran125 <ag3116213@gmail.com>
2024-09-05 19:39:35 +0800

    Membuat System.out.print dan System.out.printf

---
+++ Day04.java
@@ -0,0 +1,13 @@
+package Coding100Days;
+public class Day04 {
+    public static void main(String[] args) {
+        System.out.print("Nama: Ahmad Gibran \n");
+        System.out.print("Nim: D0224330 \n");
+        System.out.print("Kelas: D \n");
+        
+        
+        
+        System.out.printf("Nama: %s | Umur = %d | Tinggi_badan = %d cm%n | Berat_badan = %d kg%n" , "Ahmad Gibran", 18, 165, 47);
+    }
+    
+}
