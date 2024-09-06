d715cd0cbb469332de11d982357bba1f5a992307
AhmadGibran125 <ag3116213@gmail.com>
2024-09-06 19:03:37 +0800

    Membuat Komentar

---
+++ Day05.java
@@ -0,0 +1,13 @@
+ package Coding100Days;
+public class Day05 {
+    public static void main(String[] args) {
+        System.out.println("Selamat Datang"); /** 
+                                                * pesan sambutan
+                                                */
+    
+    // Sambut Mada depan yang cerah
+            /*
+            * Kejujuran 
+            */
+}
+}
