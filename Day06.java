1f07e0e7bed44cdd3990c639dfbe522e399564f5
AhmadGibran125 <ag3116213@gmail.com>
2024-09-07 19:47:52 +0800

    Membuat Variable Deklarasi

--- Day06.java
+++ Day06.java
@@ -2,14 +2,11 @@ package Coding100Days;
 public class Day06 {
     public static void main(String[] args) {
         
-        String nama = "Ahmad Gibran"; //Deklarasi String
-        int umur = 18;                //Deklarasi Integral
-        double tinggi = 165.10;       //Deklarasi Double 
-        char inisial = 'G';           //Deklarasi karakter
+        String nama;       //Deklarasi String
+        int umur;          //Deklarasi Integral
+        double tinggi;     //Deklarasi Double 
+        char inisial;      //Deklarasi karakter
         
-        
-       //Output
-        System.out.printf("Nama: %s\nUmur: %d Tahun\nTinggi: %s cm\nInisial: %s\n " ,nama,umur,tinggi,inisial);
     }
 }
  
