2fbb838a3a9898016b77463be02c8868d885dde0
AhmadGibran125 <ag3116213@gmail.com>
2024-09-09 18:10:36 +0800

    Membuat Variable Update

---
+++ Day08.java
@@ -0,0 +1,21 @@
+package Coding100Days;
+public class Day08 {
+    public static void main(String[] args) {
+        //Variable Update
+        
+        //Point awal
+        int point = 8;
+        
+        //Output
+        System.out.println("Point awal : " + point );
+        
+        //update harga
+        point += 2;
+      
+        
+        //Output
+        System.out.println("Total point : " + point);
+        
+    }
+    
+}
