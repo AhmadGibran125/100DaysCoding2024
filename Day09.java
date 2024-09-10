8fd7c757a01fa1e6869ab7757d2e644eb1cc25af
AhmadGibran125 <ag3116213@gmail.com>
2024-09-10 21:32:45 +0800

    Variable Constanta

---
+++ Day09.java
@@ -0,0 +1,20 @@
+package Coding100Days;
+
+public class Day09 { 
+    public static void main(String[] args) {
+        
+    //Constanta nilai PI
+    final double PI = 3.14;
+    //Constanta nilai MAX
+    final int nilai_MAX = 10;
+        
+        System.out.println("Nilai PI : " + PI);
+       
+        System.out.println("Nilai MAX : " + nilai_MAX );
+        
+        
+        
+   
+    }
+}
+
