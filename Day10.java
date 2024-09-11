6a66b9d13685065d23554646ecd6c7d3cb6c0e80
AhmadGibran125 <ag3116213@gmail.com>
2024-09-11 16:56:43 +0800

    Membuat tipe data byte

---
+++ Day10.java
@@ -0,0 +1,14 @@
+package Coding100Days;
+public class Day10 {
+    public static void main(String[] args) {
+        
+        byte a = 10;
+        byte b = 20;
+        byte c = 30;
+        
+        System.out.println(" nilai a : " + (c-b));
+        System.out.println(" nilai b : " + (c-a));
+        System.out.println(" nilai c : " + (a+b));
+    }
+    
+}
