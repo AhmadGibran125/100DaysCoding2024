68f536b7e967b04cba3f61b53b6cfc0e9d7c0f9e
AhmadGibran125 <ag3116213@gmail.com>
2024-09-12 21:26:37 +0800

    Membuat tipe data short

---
+++ Day11.java
@@ -0,0 +1,12 @@
+package Coding100Days;
+public class Day11 {
+    public static void main(String[] args) {
+   
+        short Min = -32768;
+        short Max = 32767;
+        
+        System.out.println("Minimum = " + Min);
+        System.out.println("Maksimum = " + Max);
+        
+    }
+}
