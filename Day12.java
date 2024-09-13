7223577bc3cc7af4f8c275a4fa4dd3a7462488da
AhmadGibran125 <ag3116213@gmail.com>
2024-09-13 17:17:55 +0800

    Membuat Tipe Data Integer

---
+++ Day12.java
@@ -0,0 +1,12 @@
+package Coding100Days;
+public class Day12 {
+    public static void main(String[] args) {
+        int Min = -2142483648;
+        int Max = 2142483647;
+        
+        System.out.println("Minimum = " + Min);
+        System.out.println("Maksimum = " + Max);
+                
+    }
+ 
+}
