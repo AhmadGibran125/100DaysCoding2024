3a2705508a456da23e097922de95572b504e9f7e
AhmadGibran125 <ag3116213@gmail.com>
2024-09-15 20:50:12 +0800

    Membuat Tipe data Long

---
+++ Day14.java
@@ -0,0 +1,11 @@
+package Coding100Days;
+
+public class Day14 {
+    public static void main(String[] args) {
+        long min = 500;
+        long max = 800;
+        //agar lolos utbk kita harus ada di rentang nilai berikut:
+        System.out.println("Nilai Minimum utbk = "+ min);
+        System.out.println("Nilai Maksimum utbk = "+ max);
+    }
+}
