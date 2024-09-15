dda69c2341a266237ed9991dc7460fcb3786105c
AhmadGibran125 <ag3116213@gmail.com>
2024-09-15 22:20:53 +0800

    Membuat tipe data Long

--- Day14.java
+++ Day14.java
@@ -2,8 +2,8 @@ package Coding100Days;
 
 public class Day14 {
     public static void main(String[] args) {
-        long nilai1 = -9.223.372.036.864.775.808L;
-        long nilai2 = 9.223.372.036.854.775.807L;
+        long nilai1 = -9223372036854775808L;
+        long nilai2 = 9223372036854775807L;
         //agar lolos utbk kita harus ada di rentang nilai berikut:
         System.out.println("Nilai Minimum = "+ nilai1);
         System.out.println("Nilai Maksimum = "+ nilai2);
