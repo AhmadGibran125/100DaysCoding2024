57f78ea422701fa548dcf97c55d8c5e5a655a5da
AhmadGibran125 <ag3116213@gmail.com>
2024-09-16 21:00:00 +0800

    Membuat tipe data long

---
+++ Day15.java
@@ -0,0 +1,11 @@
+package Coding100Days;
+public class Day15 {
+    public static void main(String[] args) {
+        double tinggi = 165.1550000000;
+        double berat = 47.7520000000;
+        
+        System.out.println("Tinggi saya : " + tinggi + "cm");
+        System.out.println("Berat saya : " + berat + "kg");
+        
+    }
+}
