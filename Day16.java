37bad79b1503250fa9a35b6feeb090415ebaf401
AhmadGibran125 <ag3116213@gmail.com>
2024-09-17 14:28:45 +0800

    Membuat Tipe data Float

---
+++ Day16.java
@@ -0,0 +1,11 @@
+package Coding100Days;
+
+public class Day16 {
+    public static void main(String[] args) {
+        float sepatu = 40.7f;
+        float sendal = 41.5f;
+        
+        System.out.println("Ukuran Sepatu :" + sepatu +"cm");
+        System.out.println("Ukuran Sendal :" + sendal +"cm");
+    }
+}
