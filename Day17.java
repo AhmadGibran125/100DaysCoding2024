45261d6138ce064e1d9e465da9f94a5f18eae0b0
AhmadGibran125 <ag3116213@gmail.com>
2024-09-18 22:20:01 +0800

    Membuat tipe data char

---
+++ Day17.java
@@ -0,0 +1,10 @@
+package Coding100Days;
+
+public class Day17 {
+    public static void main(String[] args) {
+        char inisial1 = 'A';
+        char inisial2 = 'G';
+        
+        System.out.println("saya ber inisial :" + inisial1 + inisial2);
+    }
+}
