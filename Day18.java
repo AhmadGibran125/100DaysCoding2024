63a915ac97afe9ad4a2c11b2fc3f369e0ea48d3c
AhmadGibran125 <ag3116213@gmail.com>
2024-09-19 18:44:29 +0800

    Membuat tipe data boolean

---
+++ Day18.java
@@ -0,0 +1,13 @@
+
+package Coding100Days;
+
+public class Day18 {
+    public static void main(String[] args) {
+         boolean durian = false;
+         boolean semangka = true;
+         
+         System.out.println("saya suka durian "+ durian );
+         System.out.println("saya suka semangka "+ semangka);
+         
+    }
+}
