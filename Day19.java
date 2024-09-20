3e3e4d5494e4bc13327e99b92dd0e034f5d7e800
AhmadGibran125 <ag3116213@gmail.com>
2024-09-20 16:28:24 +0800

    Membuat tipe data Referensi : String

---
+++ Day19.java
@@ -0,0 +1,15 @@
+package Coding100Days;
+
+public class Day19 {
+
+    public static void main(String[] args) {
+        String a = " apel ";
+        String b = " mangga";
+        String d = "dan";
+        String c = a+d+b;
+
+        System.out.println("saya suka makan" + c);
+      
+    }
+
+}
