f95c6ee58534908948e17fb3cc9003bff7049765
AhmadGibran125 <ag3116213@gmail.com>
2024-09-21 18:00:08 +0800

    Membuat Tipe data Referensi : Byte

---
+++ Day20.java
@@ -0,0 +1,10 @@
+package Coding100Days;
+
+public class Day20 {
+    public static void main(String[] args) {
+        Byte nilai_a = 10;
+        Byte nilai_b = 50;
+        
+        System.out.println("berapa jumlah nilai "+( nilai_a+nilai_b));
+    }
+}
