be26c05ead8796dce04c26dc3dd968088ba8da4d
AhmadGibran125 <ag3116213@gmail.com>
2024-09-22 23:25:41 +0800

    Membuat tipe data Referensi : Short

---
+++ Day21.java
@@ -0,0 +1,12 @@
+package Coding100Days;
+
+public class Day21 {
+    public static void main(String[] args) {
+        Short nilai_a = 100;
+        Short nilai_b = 50;
+        
+        System.out.println("jika saya pintar : " +nilai_a);
+        System.out.println("jika saya bodoh : " +nilai_b);
+        
+    }
+}
