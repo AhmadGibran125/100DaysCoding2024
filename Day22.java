83f474ed716d2bdffb296b1bcabae72aedb4c7c9
AhmadGibran125 <ag3116213@gmail.com>
2024-09-23 22:42:39 +0800

    Membuat Tipe data Referensi : Integer

---
+++ Day22.java
@@ -0,0 +1,20 @@
+
+package Coding100Days;
+
+public class Day22 {
+    public static void main(String[] args) {
+        
+        
+        Integer min = -2147483648;
+        Integer max = 2147483647;
+        Integer sya = Integer.valueOf(10);
+        
+        
+        System.out.println("Maksimum: "+ max);
+        
+        System.out.println("rating me :"+sya +"dari" +sya);
+        
+        System.out.println("Minimum "+ min);
+        
+    }
+}
