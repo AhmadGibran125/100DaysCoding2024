1bcbcec6eeb059442d27b1452fa9440b63906e05
AhmadGibran125 <ag3116213@gmail.com>
2024-09-24 00:24:20 +0800

    Membuat tipe data referesi : integer

--- Day22.java
+++ Day22.java
@@ -7,14 +7,19 @@ public class Day22 {
         
         Integer min = -2147483648;
         Integer max = 2147483647;
-        Integer sya = Integer.valueOf(10);
         
+        int a = 10;
+        Integer sya = Integer.valueOf(a);
         
+        String b = "20";
+        Integer me = Integer.valueOf(b);
+       
         System.out.println("Maksimum: "+ max);
+        System.out.println("Minimum: "+ min);
         
-        System.out.println("rating me :"+sya +"dari" +sya);
-        
-        System.out.println("Minimum "+ min);
+        System.out.println("konversi: "+ sya);
+        System.out.println("konversi: "+ me);
+        System.out.println("konversi: "+ (sya+me));
         
     }
 }
