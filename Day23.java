d9e84153499d8dfb672dd15b38fef0cbd1d9204a
AhmadGibran125 <ag3116213@gmail.com>
2024-09-24 22:03:01 +0800

    Membuat Tipe data referensi : Long

---
+++ Day23.java
@@ -0,0 +1,18 @@
+package Coding100Days;
+
+public class Day23 {
+
+    public static void main(String[] args) {
+        Long max = Long.MAX_VALUE;
+        Long min = Long.MIN_VALUE;
+
+        System.out.println("Maksimum Long :" + max);
+        System.out.println("Minimum Long :" + min);
+
+        String b = "1000000000000000000";
+
+        Long c = Long.valueOf(b);
+        System.out.println("konversi :" + c);
+
+    }
+}
