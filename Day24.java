2c0aa4940c63f56db2233d06730f0e5804293d0b
AhmadGibran125 <ag3116213@gmail.com>
2024-09-25 20:10:09 +0800

    Membuat tipe data referesi : Float

--- Day24.java
+++ Day24.java
@@ -7,8 +7,5 @@ public class Day24 {
         Float b = 7.5f;
         Float c = a+b;
         System.out.println("Hasil :" + c);
-       
-        float d = Float.valueOf(c);
-        System.out.println("primitif :"+d);
     }
 }
