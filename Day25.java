4b3a21892b03d00cb85a2810d8a1d46a138f60f9
AhmadGibran125 <ag3116213@gmail.com>
2024-09-26 22:32:25 +0800

    Membuat tipe data referensi : Double

---
+++ Day25.java
@@ -0,0 +1,24 @@
+
+package Coding100day;
+
+public class Day25 {
+    public static void main(String[] args) {
+        
+        Double min = Double.MIN_VALUE;
+        Double max = Double.MAX_VALUE;
+        
+        System.out.println(min);
+        System.out.println(max);
+        
+        Double a = 12.22;
+        Double b = 17.78;
+        Double c = a+b;
+        System.out.println("Jumlah :"+c);
+        
+        String ag = "31.0106";
+        Double k = Double.parseDouble(ag);
+        
+        System.out.println("konversi : "+ k);
+                
+    }
+}
