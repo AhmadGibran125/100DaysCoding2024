2ca3a78d4edcdeba85aa490ddaa2ea861474e125
AhmadGibran125 <ag3116213@gmail.com>
2024-09-27 20:25:41 +0800

    Membuat tipe data referensi : Character

---
+++ Day26.java
@@ -0,0 +1,18 @@
+
+package Coding100day;
+
+public class Day26 {
+    public static void main(String[] args) {
+            Character inisial = 'a';
+            Character name = 'g';
+            
+            System.out.println("inisial name :"+ inisial + name );
+            
+            char ag = 'b';
+            if (Character.isDigit(ag)){
+                System.out.println(ag + "adalah digit.");
+            }else{
+                System.out.println(ag + "bukan digit.");
+            }
+    }
+}
