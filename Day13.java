1a4a2eb0d28d0c2d8088005502636a33f6b2b52f
AhmadGibran125 <ag3116213@gmail.com>
2024-09-28 23:16:17 +0800

    EVALUASI MENTOR

--- Day13.java
+++ Day13.java
@@ -1,13 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Coding100Days;
 
-/**
- *
- * @author ACER
- */
 public class Day13 {
-    
+ public static void main(String[] args) {
+        byte a = 10;
+        byte b = 20;
+
+        short c = 30;
+        short d = 40;
+
+        int e = 50;
+        int f = 60;
+
+        System.out.println("nilai" + a);
+        System.out.println("nilai" + b);
+        System.out.println("nilai" + c);
+        System.out.println("nilai" + d);
+        System.out.println("nilai" + e);
+        System.out.println("nilai" + f);
+
+        System.out.printf("a :%d\nb :%d\nc :%d\nd :%d\ne :%d\nf :%d\n ", a, b, c, d, e, f);
+}
 }
