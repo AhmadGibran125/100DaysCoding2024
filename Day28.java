85a5863c3c9061c58d1e13bca48ae9108b35e41d
AhmadGibran125 <ag3116213@gmail.com>
2024-09-29 20:58:14 +0800

    Membuat tipe data referensi : Boolean

--- Day28.java
+++ Day28.java
@@ -3,17 +3,11 @@ package Coding100day;
 
 public class Day28 {
     public static void main(String[] args) {
-        Boolean a = true;
-        Boolean b = false;
+        Boolean a = true;//Sudah
+        Boolean b = false;//Belum
         
-        System.out.println("Gibran sudah kerja tugas PSTI "+a);
-        System.out.println("Gibran sudah kerja tugas Fisika "+a);
-        
-        
-        String we = "sudah";
-        Boolean d = Boolean.valueOf(we);
-        
-        System.out.println("apakah sekrang gibran kerja tugas "+d);
+        System.out.println("apakah Gibran sudah kerja tugas PSTI "+b);
+        System.out.println("apakah Gibran sudah kerja tugas Fisika "+b);
         
     }
 }
