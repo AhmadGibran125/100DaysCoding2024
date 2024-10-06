64f5ca2a0da15375ecdde0692f9fa97140b0a8c4
Gibran the villain -_- <ag3116213@gmail.com>
2024-10-06 11:48:34 +0800

    Operator : Penjumlahan dan Pengurangan

--- Day35.java
+++ Day35.java
@@ -1,17 +1,13 @@
 
 package Coding100day;
-
 import java.util.Scanner;
-
 public class Day35 {
     public static void main(String[] args) {
         Scanner gibran = new Scanner(System.in);
         
         System.out.print("NilaiA :");
-        
-        System.out.print("NilaiB :");
-        
         int A = gibran.nextInt();
+        System.out.print("NilaiB :");
         int B = gibran.nextInt();
         
         int c = A+B;
