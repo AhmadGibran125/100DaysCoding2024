afc06fdc63aa7b7ac23899e451942480d19ffeb0
Gibran the villain -_- <ag3116213@gmail.com>
2024-10-18 20:58:17 +0800

    Percabangan : if

---
+++ Day47.java
@@ -0,0 +1,17 @@
+
+package Coding100day;
+
+import java.util.Scanner;
+
+public class Day47 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        System.out.print("jika ");
+        int a = in.nextInt();
+        if (a > 0) {
+            System.out.println("Ya... "+a+" itu Nilai Positif ");
+        }
+        
+    }
+}
