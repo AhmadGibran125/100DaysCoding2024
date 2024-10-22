6583396c6ed0e6062dc7e72d6095b963f2ab64e6
Gibran the villain -_- <ag3116213@gmail.com>
2024-10-22 21:54:39 +0800

    Membuat : Switch case

---
+++ Day51.java
@@ -0,0 +1,31 @@
+
+package Coding100day;
+
+import java.util.Scanner;
+
+public class Day51 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        String huruf = in.nextLine();
+        int angka = Integer.parseInt(huruf);
+        switch (angka){
+            case 01 :
+                System.out.println("Tanggal lahir saya");
+                break ;
+            case 31 :
+                System.out.println("Bulan lahir saya");
+                break ;
+            case 2006 :
+                System.out.println("Tahun lahir saya");
+                break ;
+            default :
+                System.out.println("mana gue tau");
+                
+                
+            
+               
+        
+    }
+    }
+}
