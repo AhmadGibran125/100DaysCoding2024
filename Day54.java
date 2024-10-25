7d53a303e2739941414f90f86b9d20ebcc971e92
Gibran the villain -_- <ag3116213@gmail.com>
2024-10-25 23:40:12 +0800

    Looping : for increment

---
+++ Day54.java
@@ -0,0 +1,16 @@
+
+package Coding100day;
+
+public class Day54 {
+    public static void main(String[] args) {
+        System.out.println("Bilangan Genap");
+        for (int angka = 1; angka <= 10; angka+=2) {
+            System.out.println(angka);   
+        }
+        System.out.println("Bilangan Ganjil");
+        for (int angka = 0; angka <= 10; angka+=2) {
+            System.out.println(angka);   
+        }
+        
+    }
+}
