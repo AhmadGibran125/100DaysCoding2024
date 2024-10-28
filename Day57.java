2bb33ba360058b52d19934b4b7177559f1909385
Gibran the villain -_- <ag3116213@gmail.com>
2024-10-28 22:48:29 +0800

    while increment

---
+++ Day57.java
@@ -0,0 +1,17 @@
+
+package Coding100day;
+
+public class Day57 {
+    public static void main(String[] args) {
+        int angka = 1;
+        
+        // Looping menggunakan while 
+        while (angka < 10){
+            System.out.println("Nilai awal :"+angka);
+            System.out.println("Niali Angka : " + angka);
+            angka++; //increment  
+        }
+        System.out.println("Looping Sudah Selesai Nilai akhir : "+ angka);
+    }
+            
+}
