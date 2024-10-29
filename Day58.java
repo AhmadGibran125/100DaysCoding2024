32dd5147304aaf7172480e8e0a92c1ba696bfff3
Gibran the villain -_- <ag3116213@gmail.com>
2024-10-29 20:54:30 +0800

    while decrement

---
+++ Day58.java
@@ -0,0 +1,23 @@
+
+package Coding100day;
+
+public class Day58 {
+    public static void main(String[] args) {
+       int angka = 10;
+       
+       while(angka > 0){
+           System.out.println("Nilai Angka "+angka);
+           angka--; //Decrement
+           
+       }
+       
+        System.out.println("Kelipatan 2");
+        int nilai = 20;
+        while (nilai >= 0){
+            System.out.println(nilai);
+            nilai -= 5;
+        }
+        
+        
+    }
+}
