5e70a4425783d6366840f7255e23222141b41eed
Gibran the villain -_- <ag3116213@gmail.com>
2024-10-31 22:16:30 +0800

    Looping : do while

---
+++ Day60.java
@@ -0,0 +1,20 @@
+package Coding100day;
+
+public class Day60 {
+    public static void main(String[] args) {
+        
+        int angka = 0;
+        
+        do{
+            System.out.print(angka+" ");
+            angka++;    
+        }while (angka < 10);
+        
+        int angka1 = 10;
+        do{
+            System.out.print(angka1+" ");
+            angka1--;
+        }while (angka1 >= 0);
+        
+    }
+}
