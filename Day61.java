e4e4d97b1f1582fc82a7da2fd242d2d323c7c23e
Gibran the villain -_- <ag3116213@gmail.com>
2024-11-01 22:19:06 +0800

    Invinity loop

---
+++ Day61.java
@@ -0,0 +1,18 @@
+package Coding100day;
+
+public class Day61 {
+    public static void main(String[] args) {
+        
+        int ang = 10;
+        for (int i = 0; i < ang;){
+            System.out.println("ang");
+        {    
+        }          
+        }
+       
+        while (true){
+            System.out.println("gibran");
+        }
+        
+    }
+}
