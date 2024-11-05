834761c7711c2a084408dc400a6307b24bf45f15
Gibran the villain -_- <ag3116213@gmail.com>
2024-11-05 21:14:28 +0800

    nested loop

--- Day65.java
+++ Day65.java
@@ -1,28 +1,41 @@
 package Coding100day;
 
 public class Day65 {
+
     public static void main(String[] args) {
         //luar
         for (int i = 1; i <= 3; i++) {
             //dalam
             for (int j = 1; j <= 3; j++) {
-                System.out.print(j+" ");
-                
+                System.out.print(j + " ");
+
             }
             System.out.println("");
         }
         int a = 3;
         //luar
-        while (a >= 1){
+        while (a >= 1) {
             int b = 3;
             //dalam
-            while (b >= 1){
-                System.out.print(b+" ");
+            while (b >= 1) {
+                System.out.print(b + " ");
                 b--;
             }
             System.out.println("");
             a--;
         }
-        
+        int ag = 1;
+        do {
+
+            int ga = 1;
+            do {
+                ga++;
+                System.out.print("Ah ");
+            } while (ga <= 4);
+
+            ag++;
+            System.out.println();
+        } while (ag <= 4);
+
     }
 }
