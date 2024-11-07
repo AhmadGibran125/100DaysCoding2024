b67c0b01281f95fdce259047daf0a2210fd9a7ca
Gibran the villain -_- <ag3116213@gmail.com>
2024-11-07 21:51:15 +0800

    Method void

---
+++ Day67.java
@@ -0,0 +1,15 @@
+package Coding100day;
+
+public class Day67 {
+    public static void sapa(String aku) {
+        System.out.println("Halo,");
+        System.out.println("Nama saya "+aku);
+    }
+    public static void umur(int angka) {
+        System.out.println("Umur saya "+angka+" Tahun");
+    }
+    public static void main(String[] args) {
+        sapa("Gibran");
+        umur(18);
+    }
+}
