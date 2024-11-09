e2eca9c5ea126fd2e7854637a828f9f413fc5728
Gibran the villain -_- <ag3116213@gmail.com>
2024-11-09 19:06:43 +0800

    Method non void

---
+++ Day68.java
@@ -0,0 +1,12 @@
+package Coding100day;
+
+public class Day68 {
+    public int luasPersegi(int sisi ){
+    return sisi * sisi;
+}
+    public static void main(String[] args) {
+        Day68 luas = new Day68();
+        int hasil = luas.luasPersegi(10);
+        System.out.println(hasil);
+    }
+}
