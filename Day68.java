4d4866ce09986c9d9b9ddd7ae21ba3e2ba385ab5
Gibran the villain -_- <ag3116213@gmail.com>
2024-11-09 19:11:59 +0800

    Method Non Void

--- Day68.java
+++ Day68.java
@@ -1,12 +1,20 @@
 package Coding100day;
 
 public class Day68 {
+    public int angka (){
+        return 10;
+    }
     public int luasPersegi(int sisi ){
-    return sisi * sisi;
+        return sisi * sisi;
 }
     public static void main(String[] args) {
         Day68 luas = new Day68();
+        int Angka = luas.angka();
+        System.out.print("Mngembalikkan angka ");
+        System.out.println(Angka);
+        
         int hasil = luas.luasPersegi(10);
+        System.out.print("Luas Persegi ");
         System.out.println(hasil);
     }
 }
