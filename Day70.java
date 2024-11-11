7ccbe9121d60b59b538bf5e1da4c3728d7873939
Gibran the villain -_- <ag3116213@gmail.com>
2024-11-11 23:43:48 +0800

    Method Parameter

--- Day70.java
+++ Day70.java
@@ -1,19 +1,22 @@
 package Coding100day;
 
-public class Day70 {
-    public static int apa(int sisBa) {
-        return sisBa;
+public class Day69 {
+    // Method static dengan 1 parameter
+         public static int tambah(int angka) {
+            return angka+2;
+         }
+    // Method non static dengan 2 parameter
+         public int kali(int a, int b) {
+            return a * b;
     }
-    
     public static void main(String[] args) {
+        // sataic langsung panggil tanpa new
+        int hasil1 = tambah(5);
+        System.out.println("Hasil setelah penambahan "+hasil1);
         
-        int hasil = apa(11);
-        System.out.println(hasil);
-        
-        if (hasil % 2 == 0) {
-            System.out.println("Ini GENAP");
-        }else {
-            System.out.println("Ini GANJIL");
-        }
+        // non static perlu objek dulu pakai new
+        Day69 apa = new Day69();
+        int hasil2 = apa.kali(2,10);
+        System.out.println("Hasil setelah diakli a dan b "+ hasil2);
     }
 }
