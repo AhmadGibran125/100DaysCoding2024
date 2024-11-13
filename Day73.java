d03aa1ccd26c496d2c017f2466c3131ee6fdfdaf
Gibran the villain -_- <ag3116213@gmail.com>
2024-11-13 22:57:19 +0800

    Method charAt

---
+++ Day73.java
@@ -0,0 +1,14 @@
+package Coding100day;
+
+public class Day73 {
+
+    public static void main(String[] args) {
+        String nama = "Gibran";
+
+        for (int i = 0; i < 6; i++) {
+
+            char a = nama.charAt(i);
+            System.out.print(a+" ");
+        }
+    }
+}
