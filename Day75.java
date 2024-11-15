5852ff131f9b3d7df58339afe9cdae64a850b8c6
Gibran the villain -_- <ag3116213@gmail.com>
2024-11-15 22:49:39 +0800

    Day75

---
+++ Day75.java
@@ -0,0 +1,15 @@
+package Coding100day;
+
+import java.util.Scanner;
+
+public class Day75 {
+    public static void main(String[] args) {
+      Scanner in = new Scanner(System.in);
+      String kata = in.nextLine();
+      
+      String peArubahan = kata.toLowerCase();
+      
+        System.out.println("Before "+kata);
+        System.out.println("After "+perubahan);
+    }
+}
