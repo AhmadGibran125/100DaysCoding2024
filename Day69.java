55bebb885d3fad5012e9c0d8c56e725eaa69967f
Gibran the villain -_- <ag3116213@gmail.com>
2024-11-15 22:51:30 +0800

    EVALUASI MENTOR

--- Day69.java
+++ Day69.java
@@ -1,1 +1,26 @@
+package Coding100day;
 
+import java.util.Scanner;
+
+public class Day69 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            int a = in.nextInt();
+            if (a != 9) {
+                System.out.println("Masukkan Lagi Angka ");
+            } else if (a == 9) {
+                System.out.println("Program Dihentikan ");
+                break;
+            }
+        }
+
+        int b;
+        do {
+            System.out.println("masukkan angka");
+            b = in.nextInt();
+        } while (b != 9);
+        System.out.println("program di hentikan");
+    }
+}
