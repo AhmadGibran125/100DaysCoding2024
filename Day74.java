24780aadb8e19db4763b47115aed07c004568ed8
Gibran the villain -_- <ag3116213@gmail.com>
2024-11-15 18:28:49 +0800

    String Method : lenght()

---
+++ Day74.java
@@ -0,0 +1,16 @@
+
+package Coding100day;
+
+import java.util.Scanner;
+
+public class Day74 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        String kata = in.nextLine();
+        int panjangKata = kata.length();
+        
+        System.out.println("Panjang dari kata Tersebut "+panjangKata);
+        
+    }
+}
