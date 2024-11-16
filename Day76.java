8308bbfdd2edc566e31fa80aa76aee212d1049e1
Gibran the villain -_- <ag3116213@gmail.com>
2024-11-16 15:41:51 +0800

    Method String toUpperCase

---
+++ Day76.java
@@ -0,0 +1,15 @@
+package Coding100day;
+
+import java.util.Scanner;
+
+public class Day76 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        String kata = in.nextLine();
+        String perubahan = kata.toUpperCase();
+        System.out.println("Seteleah di UpperCase : "+perubahan);
+        
+                
+    }
+}
