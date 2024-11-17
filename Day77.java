699cd51e03715b724dddc72f8c5ac975a25834f9
Gibran the villain -_- <ag3116213@gmail.com>
2024-11-17 20:43:44 +0800

    String Method equals

---
+++ Day77.java
@@ -0,0 +1,14 @@
+
+package Coding100day;
+
+public class Day77 {
+    public static void main(String[] args) {
+        String pertama = "helo";
+        String kedua = "Helo";
+        String ketiga = "dunia";
+        
+        System.out.println(pertama.equals(kedua));
+        System.out.println(pertama.equals(ketiga));
+    }
+    
+}
