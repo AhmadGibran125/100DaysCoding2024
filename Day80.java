e27f18f98d04e98533ef5348961da29b8b235ac2
Gibran the villain -_- <ag3116213@gmail.com>
2024-11-20 11:31:38 +0800

    String Method : trim()

---
+++ Day80.java
@@ -0,0 +1,16 @@
+package Coding100Day;
+
+public class Day80 {
+
+    public static void main(String[] args) {
+        
+        String nama = "          - Gibran -            ";
+        System.out.println("Before : "+nama);
+        String name = nama.trim();
+        System.out.println("After : "+name);
+        
+        
+        
+    }
+
+}
