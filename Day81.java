c5a7ededf8b2548a8342727a23eed8c90fcf9182
Gibran the villain -_- <ag3116213@gmail.com>
2024-11-21 22:49:59 +0800

    String Method : valueOf()

---
+++ Day81.java
@@ -0,0 +1,14 @@
+package Coding100Days;
+public class Day81 {
+    public static void main(String[] args) {
+        int apa = 100;
+        String str = String.valueOf(apa);
+        System.out.println("Ini adalah angka "+apa);
+        System.out.println("Ini adalah karakter dari string "+str);
+        
+        apa += 1;
+        System.out.println("ini adalah angka "+apa);
+        str += 1;
+        System.out.println("Ini adalah Krakter "+str);
+    }
+}
