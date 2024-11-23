82f2a1108d4452271b93bf2861ee16cbd45f70d9
Gibran the villain -_- <ag3116213@gmail.com>
2024-11-24 00:18:06 +0800

    Array 1 Dimensi

---
+++ Day83.java
@@ -0,0 +1,13 @@
+
+package Coding100day;
+
+public class Day83 {
+    public static void main(String[] args) {
+        String[] hewan = { "Harimau" , "Singa" , "Buaya" }; 
+        
+        for (String Hewan : hewan) {
+            System.out.print(Hewan);
+        }
+       
+    }
+}
