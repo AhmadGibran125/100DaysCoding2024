436f778efa0c737d143db6ac3527ddec1b43d120
Gibran the villain -_- <ag3116213@gmail.com>
2024-11-25 23:34:10 +0800

    Mengisi elemen ke Array satu dimensi

---
+++ Day85.java
@@ -0,0 +1,17 @@
+
+package Coding100day;
+
+
+
+public class Day85 {
+    public static void main(String[] args) {
+       int[] angka = {1,2,3,4,5};
+       
+        for (int i = 0; i < angka.length; i++) {
+            System.out.println(angka[i]);
+        }
+            for (int in  : angka) {
+                System.out.println(in +" ");
+        }
+    }
+}
