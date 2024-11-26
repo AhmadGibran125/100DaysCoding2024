b55ee815fe1535a352d501968f94b9d2996135f2
Gibran the villain -_- <ag3116213@gmail.com>
2024-11-27 00:44:22 +0800

    Mencetak elemen pada array 1 dimensi

--- Day86.java
+++ Day86.java
@@ -1,4 +1,3 @@
-
 package Coding100day;
 
 import java.util.Arrays;
@@ -8,23 +7,24 @@ public class Day86 {
         
         System.out.println(Arrays.toString(yaaa));
         
-        yaaa[5] = 1;
-        yaaa[4] = 2;
+        yaaa[0] = 1;
+        yaaa[1] = 2;
         yaaa[2] = 3;
-        yaaa[6] = 4;
-        yaaa[1] = 5;
-        yaaa[0] = 6;
-        yaaa[3] = 7;
+        yaaa[3] = 4;
+        yaaa[4] = 5;
+        yaaa[5] = 6;
+        yaaa[6] = 7;
         yaaa[7] = 8;
         
-        System.out.println(yaaa[5]);
-        System.out.println(yaaa[4]);
-        System.out.println(yaaa[2]);
-        System.out.println(yaaa[6]);
-        System.out.println(yaaa[1]);
         System.out.println(yaaa[0]);
+        System.out.println(yaaa[1]);
+        System.out.println(yaaa[2]);
         System.out.println(yaaa[3]);
+        System.out.println(yaaa[4]);
+        System.out.println(yaaa[5]);
+        System.out.println(yaaa[6]);
         System.out.println(yaaa[7]);
+       
         
         for (int i = 0; i < yaaa.length; i++) {
             System.out.println("Elemen "+i+" : "+ yaaa[i]);
