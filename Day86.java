a491b94d24b3d31ca8a0c91fd52669e616026246
Gibran the villain -_- <ag3116213@gmail.com>
2024-11-27 00:25:31 +0800

    Mencetak Elemen pada Array

---
+++ Day86.java
@@ -0,0 +1,34 @@
+
+package Coding100day;
+
+import java.util.Arrays;
+public class Day86 {
+    public static void main(String[] args) {
+        int[] yaaa = {6,5,3,7,2,1,4,8};
+        
+        System.out.println(Arrays.toString(yaaa));
+        
+        yaaa[5] = 1;
+        yaaa[4] = 2;
+        yaaa[2] = 3;
+        yaaa[6] = 4;
+        yaaa[1] = 5;
+        yaaa[0] = 6;
+        yaaa[3] = 7;
+        yaaa[7] = 8;
+        
+        System.out.println(yaaa[5]);
+        System.out.println(yaaa[4]);
+        System.out.println(yaaa[2]);
+        System.out.println(yaaa[6]);
+        System.out.println(yaaa[1]);
+        System.out.println(yaaa[0]);
+        System.out.println(yaaa[3]);
+        System.out.println(yaaa[7]);
+        
+        for (int i = 0; i < yaaa.length; i++) {
+            System.out.println("Elemen "+i+" : "+ yaaa[i]);
+        }
+    }
+    
+}
