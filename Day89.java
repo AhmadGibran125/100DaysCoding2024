da1c3d6b82ecd8cb07b909e5531cb967f4fb40a7
Gibran the villain -_- <ag3116213@gmail.com>
2024-11-30 00:24:14 +0800

    Soal2

---
+++ Day89.java
@@ -0,0 +1,41 @@
+package Coding100day;
+
+import java.util.Scanner;
+
+public class Day89 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Mau Berapa Berapa Elemen kak..? ");
+        
+        
+        int kon = in.nextInt();
+        in.nextLine();
+        
+        String[] ar = new String[kon];
+        for (int i = 0; i < ar.length; i++) {
+            System.out.print(i+1+" . ");
+            ar [i] = in.nextLine();
+        }
+        System.out.println("Cetak");
+        for (int i = 0; i < ar.length; i++) {
+            System.out.println(i+1+" . "+ar[i]);
+        }
+        System.out.print("Mau ubah indeks berapa kak ? ");
+        int inde = in.nextInt();
+        in.nextLine();
+        inde -=1;
+        if (inde >= 0 && inde < kon) {
+            
+            System.out.print(inde+1+" . ");
+             ar[inde] = in.nextLine();
+             System.out.println("Proses.....");
+        }else {
+            System.out.println("not valid");
+        }
+        
+        System.out.println("Pembaharuan ");
+        for (int i = 0; i < ar.length; i++) {
+            System.out.println((i+1)+ " . "+ ar[i]);
+        }
+    }
+}
